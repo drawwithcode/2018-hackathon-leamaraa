6a2c45116a44fb0915e976367bebba25e18c635f
leamaraa <43402546+leamaraa@users.noreply.github.com>
2018-11-28 12:32:46 +0100

    Update particle.js

--- particle.js
+++ particle.js
@@ -1,27 +1,42 @@
-function Particle (){
-  this.pos = createVector(random(width),random(height));
-  this.vel = p5.Vector.random2D();
-  this.acc = createVector(0,0);
+function Particle() {
+  this.pos = createVector(random(width), random(height));
+  this.vel = createVector(0,0);
+  this.acc = createVector(0, 0);
+  this.maxspeed = 4;
+  this.prevPos = this.pos.copy();
 
-  this.update = function(){
+  this.update = function() {
     this.vel.add(this.acc);
+    this.vel.limit(this.maxspeed);
     this.pos.add(this.vel);
     this.acc.mult(0);
   }
 
-  this.applyForce = function(force){
+this.follow = function (vectors){
+  var x = floor(this.pos.x/scl);
+  var y = floor(this.pos.y/scl);
+  var index = x + y * cols;
+  var force = vectors [index];
+  this.applyForce(force);
+}
+
+  this.applyForce = function(force) {
     this.acc.add(force);
   }
 
-  this.show = function(){
-    stroke(0);
-    point(this.pos.x, this.pos.y);
+  this.show = function() {
+    stroke(255, 50);
+    strokeWeight(1);
+    line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
+    // point(this.pos.x, this.pos.y);
   }
 
-  this.edges = function (){
-    if (this.pos.x > width) this.pos.x = 0;
-    if (this.pos.x > 0) this.pos.x = width;
-    if (this.pos.y > height) this.pos.y = 0;
-    if (this.pos.y > 0) this.pos.y = height;
+  this.edges = function(){
+if (this.pos.x > width) this.pos.x = 0;
+if (this.pos.x < 0) this.pos.x = width;
+if (this.pos.y > height) this.pos.y = 0;
+if (this.pos.y < 0) this.pos.y = height;
+
   }
+
 }
